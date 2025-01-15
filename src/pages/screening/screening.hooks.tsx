c85ea14220ba3b51b26b3972f83c3d7aa16df89f
Mark Allen <mark.a.allen@gmail.com>
2025-01-15 15:46:40 -0500

    Screening updates for institution referrers

--- src/pages/screening/screening.hooks.tsx
+++ src/pages/screening/screening.hooks.tsx
@@ -148,6 +148,17 @@ export function useScreeningNavigation() {
 				case ScreeningSessionDestinationId.INSTITUTION_REFERRAL:
 					window.location.href = `${destination.context.institutionReferralUrl}`;
 					return;
+				case ScreeningSessionDestinationId.INSTITUTION_REFERRER_DETAIL:
+					revalidator.revalidate();
+					navigate(
+						{
+							pathname: `/referrals/${destination.context.institutionReferrerUrlName}`,
+						},
+						{
+							replace,
+						}
+					);
+					return;
 				case ScreeningSessionDestinationId.ONE_ON_ONE_PROVIDER_LIST:
 				default: {
 					navigate(
