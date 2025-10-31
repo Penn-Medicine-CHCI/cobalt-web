3601f83cb598083abddd7a89de65246c9b3db6da
Adam Grayson <asgrayson90@gmail.com>
2025-10-31 09:15:40 -0400

    remove unused var

--- src/routes/admin/pages/pages.tsx
+++ src/routes/admin/pages/pages.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Badge, Button, Col, Container, Row } from 'react-bootstrap';
 import { PAGE_STATUS_ID, PageDetailModel } from '@/lib/models';
 import { pagesService } from '@/lib/services';
